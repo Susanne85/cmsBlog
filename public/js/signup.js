606795013e8c9c0e49ec6dcbe700253c4c691269
Susanne85 <xkqk2010@gmail.com>
2021-06-14 23:12:45 +0800

    Added more meaningful error messages on signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,35 +1,43 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      try{
-        const response = await fetch('/api/users', {
-          method: 'POST',
-          body: JSON.stringify({ username, email, password }),
-          headers: { 'Content-Type': 'application/json' },
-        });
+  event.preventDefault();
 
-        if (response.ok) {
-          document.location.replace('/');
-        } else {
-          alert('User already exists, please click on login');
-        }
-      }catch (error) {
-       response.status(500).json(error);
-      }
-    }else {
-      alert('Enter email and password or click on login');
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    if (!email.includes('@')) {
+      alert('Email address must contain an @ symbol');
+      return;
     }
-  };
-  
-  const loginFormHandler = async (event) => {
-    event.preventDefault();
-    document.location.replace('/login');
-  }
+    if (password.length < 6) {
+      alert('Length of password must be 6 or more characters');
+      return;
+    }
+    try {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
 
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('User already exists, please click on login');
+      }
+    } catch (error) {
+      response.status(500).json(error);
+    }
+  } else {
+    alert('User name, email and password must all be entered');
+  }
+};
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  document.location.replace('/login');
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
